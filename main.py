24e81049ad3dbf261f8b905efe0d12e41497f4d1
Alejandro Anachuri <alejandroanachuri@gmail.com>
2024-08-28 22:08:43 -0300

    main updated

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI  
+from fastapi import FastAPI, File, UploadFile 
+import vercel_blob
+
 
 app = FastAPI() 
 
 @app.get("/")
 def read_root():
     return {"message": "¡Hola mundo!"}
+
+@app.post('/upload')
+def upload(file:UploadFile):
+    vercel_blob.put(file.filename, file.read(), {})
+    return {"message": "Archivo recibido"}
